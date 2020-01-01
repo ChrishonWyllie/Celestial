fe5dedd49c321fa264c7b7095b44ca5f3f40008a
Chrishon <chrishon595@yahoo.com>
2019-12-31 21:22:43 -0500

    Added public access modifier to required initializer

--- Classes/URLImageView/URLImageView.swift
+++ Classes/URLImageView/URLImageView.swift
@@ -39,7 +39,7 @@ open class URLImageView: UIImageView {
         self.defaultImage = defaultImage
     }
     
-    required init?(coder: NSCoder) {
+    required public init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
