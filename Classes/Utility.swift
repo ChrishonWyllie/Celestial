ecf4f7a856c081629b54870807a008be8bada912
Chrishon <chrishon595@yahoo.com>
2020-06-20 18:51:18 -0400

    Created Utility class. Will eventually contain more functions

--- Classes/Utility.swift
+++ Classes/Utility.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+internal typealias DownloadProgress = Float
+internal typealias DownloadPercentage = String
+
+internal class Utility {
+    
+    public static let shared = Utility()
+    
+    
+    
+    
+    
+    
+    private init() {
+        
+    }
+    
+    func getDownloadProgress(totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) -> (DownloadProgress, DownloadPercentage) {
+        let downloadProgress: DownloadProgress = Float(totalBytesWritten) / Float(totalBytesExpectedToWrite)
+        let totalMediaObjectSize = ByteCountFormatter.string(fromByteCount: totalBytesExpectedToWrite, countStyle: .file)
+        let humanReadableDownloadProgress: DownloadPercentage = String(format: "%.1f%% of %@", downloadProgress * 100, totalMediaObjectSize)
+        
+        return (downloadProgress, humanReadableDownloadProgress)
+    }
+}
