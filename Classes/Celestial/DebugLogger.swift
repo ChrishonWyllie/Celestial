bec929b85534d02947f51712de1ed439126cf7e7
Chrishon <chrishon595@yahoo.com>
2020-06-24 18:18:13 -0400

    Added DebugLogger class

--- Classes/Celestial/DebugLogger.swift
+++ Classes/Celestial/DebugLogger.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+internal class DebugLogger {
+    
+    public static let shared = DebugLogger()
+    private var debugMessages: [String] = []
+    private var withNsLog = true
+    
+    private let serialQueue = DispatchQueue(label: "com.chrishonwyllie.Celestial.DebugLogger")
+
+    
+    
+    private init(withNsLog: Bool = true) {
+        self.withNsLog = withNsLog
+        clear()
+    }
+    
+    public func addDebugMessage(_ message: String) {
+        guard Celestial.shared.debugModeIsActive == true else {
+            return
+        }
+        if self.withNsLog {
+            #if DEBUG
+            NSLog(message)
+            #endif
+        }
+        serialQueue.sync {
+            self.debugMessages.append(message)
+        }
+        
+    }
+    
+    public func getAllMessages() -> [String] {
+        return debugMessages
+    }
+    
+    public func clear() {
+        debugMessages.removeAll()
+    }
+}
