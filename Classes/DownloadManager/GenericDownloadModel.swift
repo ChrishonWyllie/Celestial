9cbb0af9763292fc01607df2ec24fe3705bf41db
Chrishon <chrishon595@yahoo.com>
2020-06-24 00:53:30 -0400

    Added documentation for GenericDownloadModel

--- Classes/DownloadManager/GenericDownloadModel.swift
+++ Classes/DownloadManager/GenericDownloadModel.swift
@@ -7,43 +7,83 @@
 
 import Foundation
 
+/// Delegate for notifying receiver of progress, completion and possible errors
+/// of a resource located at a external URL
 internal protocol CachableDownloadModelDelegate: class {
     
-    /// This is called when the media is fully downloaded.
-    /// At this point, the data can be cached.
+    
+    /**
+     Notifies receiver that media has been finished downloading to a temporary file location.
+
+    - Parameters:
+       - downloadTaskRequest: The DownloadTaskRequest object that finished the download. Note, this object has been invalidated after completion.
+       - localTemporaryFileURL: The temporary url pointing to the downloaded resource
+    */
     func cachable(_ downloadTaskRequest: DownloadTaskRequestProtocol, didFinishDownloadingTo localTemporaryFileURL: URL)
     
-    /// This is called when an error occurred while downloading the media
-    /// Inspect the `error` argument for a more detailed description.
+    /**
+     Notifies receiver that the download has failed.
+
+    - Parameters:
+       - downloadTaskRequest: The DownloadTaskRequest object that finished the download. Note, this object has been invalidated after completion.
+       - error: Error encour
+    */
     func cachable(_ downloadTaskRequest: DownloadTaskRequestProtocol, downloadFailedWith error: Error)
     
-    /// This is called every time a new portion of data is received.
-    /// This can be used to update your UI with the appropriate values to let users know the progress of the download
+    /**
+     Notifies receiver of download progress.
+
+    - Parameters:
+       - downloadTaskRequest: The DownloadTaskRequest object that finished the download. Note, this object has been invalidated after completion.
+       - progress: The current progress of the download from 0.0 to 1.0
+       - humanReadableProgress: A easily readable version of the progress. For convenience
+    */
     func cachable(_ downloadTaskRequest: DownloadTaskRequestProtocol, downloadProgress progress: Float, humanReadableProgress: String)
 }
 
+/// Represents current state of a download of a resource from an external URL
 internal enum DownloadTaskState {
+    /// First state of a download before it begins
     case none
+    /// Download has been temporarily paused. May be resumed
     case paused
+    /// Download is currently in progress
     case downloading
+    /// Download has finished and stored to a temporary URL, waiting to be cached if desired
     case finished
 }
 
+/// Determines where cached files will be stored if `cachePolicy == .allow`
 public enum DownloadCompletionCacheStyle {
+    /// Downloaded resources will be stored in local `NSCache`
     case inMemory
+    /// Downloaded resources will be stored in local file system
     case fileSystem
 }
 
-///
+/// Model used to initiate a download and link the state of the download with its receiver.
+/// May be used to notify of updates such as progress, errors and completion
 internal protocol DownloadModelRepresentable: class {
+    /// The URL of the resource to be downloaded
     var sourceURL: URL { get }
+    /// The state of the current download. For updating UI with download state,
+    /// it is best to use the `CachableDownloadModelDelegate`
     var downloadState: DownloadTaskState { get }
+    /// Used to notify receiver of download state events such as completion, progress and errors
     var delegate: CachableDownloadModelDelegate? { get set }
     
+    /// Updates the download state
+    /**
+     Updates the download state
+
+    - Parameters:
+       - downloadState: The new current state of the download
+    */
     func update(downloadState: DownloadTaskState)
 }
 
-
+/// Model used to initiate a download and link the state of the download with its receiver.
+/// May be used to notify of updates such as progress, errors and completion
 internal class GenericDownloadModel: DownloadModelRepresentable, CustomStringConvertible {
     var sourceURL: URL
     weak var delegate: CachableDownloadModelDelegate?
@@ -59,6 +99,6 @@ internal class GenericDownloadModel: DownloadModelRepresentable, CustomStringCon
     }
     
     var description: String {
-        return "URL: \(sourceURL) \n delegate: \(String(describing: delegate)) \n downloadState: \(downloadState)"
+        return "\(String(describing: type(of: self))) - URL: \(sourceURL) \n delegate: \(String(describing: delegate)) \n downloadState: \(downloadState)"
     }
 }
