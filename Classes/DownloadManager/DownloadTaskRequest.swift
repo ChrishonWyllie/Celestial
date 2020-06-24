df2bbe7d8365b091c361000b1afce53029ad58e4
Chrishon <chrishon595@yahoo.com>
2020-06-24 01:30:00 -0400

    Added documentation for DownloadTaskRequest

--- Classes/DownloadManager/DownloadTaskRequest.swift
+++ Classes/DownloadManager/DownloadTaskRequest.swift
@@ -7,20 +7,35 @@
 
 import Foundation
 
+/// Initiates a download from a `DownloadModelRepresentable` object
 internal protocol DownloadTaskRequestProtocol {
+    /// Current progress of a download. To update UI, use `CachableDownloadModelDelegate`
     var progress: Float { get }
+    /// Partial Data as a result of pausing a download. May be used to resume if desired
     var resumeData: Data? { get }
+    /// `URLSessionDownloadTask` used to download the desired resource
     var task: URLSessionDownloadTask? { get }
-    var downloadObject: DownloadModelRepresentable { get }
+    /// Model used to initiate a download and link the state of the download with its receiver.
+    /// May be used to notify of updates such as progress, errors and completion
+    var downloadModel: DownloadModelRepresentable { get }
+    
+    /**
+     Initializes a object representing a download task as well as supporting information
+
+    - Parameters:
+       - downloadModel: The DownloadModel used to initiate the request with a source URL.
+    */
+    init(downloadModel: DownloadModelRepresentable)
 }
 
+/// Initiates a download from a `DownloadModelRepresentable` object
 internal class DownloadTaskRequest: DownloadTaskRequestProtocol {
     var progress: Float = 0
     var resumeData: Data?
     var task: URLSessionDownloadTask?
-    var downloadObject: DownloadModelRepresentable
+    var downloadModel: DownloadModelRepresentable
     
-    init(downloadObject: DownloadModelRepresentable) {
-        self.downloadObject = downloadObject
+    required init(downloadModel: DownloadModelRepresentable) {
+        self.downloadModel = downloadModel
     }
 }
