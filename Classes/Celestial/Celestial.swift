108325f8d6ea1a87d50e2693758eb969998ddc0a
Chrishon Wyllie <chrishonwyllie@Chrishons-iMac.lan>
2021-04-22 23:48:27 -0400

    Added a new class for handling the AVAsset export sessions for when a different export quality is selected

--- Classes/Celestial/Celestial.swift
+++ Classes/Celestial/Celestial.swift
@@ -22,6 +22,11 @@ public final class Celestial: NSObject {
     
     private var backgroundSessionCompletionHandler: (() -> Void)?
     
+    public enum VideoExportQuality {
+        case `default`
+        case low
+        case medium
+    }
     
     
     
