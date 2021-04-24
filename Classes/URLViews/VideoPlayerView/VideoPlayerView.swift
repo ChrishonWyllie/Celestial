95e22106c2ae49ba756def25074586100c8ab03e
Chrishon Wyllie <chrishonwyllie@Chrishons-iMac.lan>
2021-04-23 22:28:42 -0400

    Provided two properties to the VideoPlayerView to determine the video resolution and aspectRatio

--- Classes/URLViews/VideoPlayerView/VideoPlayerView.swift
+++ Classes/URLViews/VideoPlayerView/VideoPlayerView.swift
@@ -28,4 +28,14 @@ import AVFoundation
         return layer as! AVPlayerLayer
     }
     
+    /// Returns the resolution of the video in pixel format (e.g. 1080 Width by 1980 Height for a vertical video or 9:16 format)
+    /// NOTE: This will be nil or zero until the video is ready to play
+    public var resolution: CGSize? {
+        return self.player?.currentItem?.resolution
+    }
+    
+    /// Returns the aspect ratio of the video represented as a double
+    public var aspectRatio: Double? {
+        return self.player?.currentItem?.aspectRatio
+    }
 }
